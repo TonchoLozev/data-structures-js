e6e9258d315512c314d47db3e898d5e8838a2a63
Toncho <toncho.lozev@yahoo.com>
2020-10-27 21:26:04 +0200

    implement addTotTail method

--- linked-list/01.Linked-list.js
+++ linked-list/01.Linked-list.js
@@ -21,10 +21,26 @@ LinkedList.prototype.addToHead = function (value) {
     this.head = newNode;
 }
 
+LinkedList.prototype.addToTail = function (value) {
+    const newNode = new Node(value, null, this.tail);
+
+    if (this.tail){
+        this.tail.next = newNode;
+    } else {
+        this.head = newNode;
+    }
+
+    this.tail = newNode;
+}
+
 const ll = new LinkedList();
 
 ll.addToHead(100);
 ll.addToHead(200);
 ll.addToHead(300);
+
+ll.addToTail(10);
+ll.addToTail(20);
+ll.addToTail(30);
 
 console.log(ll);
