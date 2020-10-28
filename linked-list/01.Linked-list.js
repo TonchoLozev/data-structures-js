ee97c83ab98516c2a24bfac6a1d683688de5662c
Toncho <toncho.lozev@yahoo.com>
2020-10-28 07:46:21 +0200

    big o notations

--- linked-list/01.Linked-list.js
+++ linked-list/01.Linked-list.js
@@ -33,14 +33,75 @@ LinkedList.prototype.addToTail = function (value) {
     this.tail = newNode;
 }
 
+LinkedList.prototype.removeHead = function(){
+    if (!this.head) return null;
+
+    const val = this.head.value;
+    this.head = this.head.next;
+
+    if (this.head){
+        this.head.prev = null;
+    } else {
+        this.tail = null;
+    }
+
+    return val;
+}
+
+LinkedList.prototype.removeTail = function(){
+    if (!this.tail) return null;
+
+    const val = this.tail.value;
+    this.tail = this.tail.prev;
+
+    if (this.tail){
+        this.tail.next = null;
+    } else {
+        this.head = null;
+    }
+
+    return val;
+}
+
+LinkedList.prototype.search = function (searchValue) { 
+    let currentNode = this.head;
+
+    while (currentNode) {
+        if(currentNode.value === searchValue) {
+            return currentNode.value;
+        }
+        currentNode = currentNode.next;
+    }
+
+    return null;
+}
+
+LinkedList.prototype.indexOf = function (value) {
+    let currentNode = this.head;
+    let index = 0;
+    let arrayOfIndexes = [];
+
+    while (currentNode) {
+        if(currentNode.value === value) {
+            arrayOfIndexes.push(index);
+        }
+        currentNode = currentNode.next;
+        index++;
+    }
+
+    return arrayOfIndexes;
+}
+
 const ll = new LinkedList();
 
 ll.addToHead(100);
-ll.addToHead(200);
-ll.addToHead(300);
+ll.addToHead('hi');
+ll.addToTail(300);
 
 ll.addToTail(10);
-ll.addToTail(20);
+// ll.addToTail('ui');
 ll.addToTail(30);
+// ll.addToTail('ui');
 
-console.log(ll);
+
+console.log(ll.indexOf('ui'));
