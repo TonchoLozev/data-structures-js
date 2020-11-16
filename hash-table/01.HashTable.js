652c9fd83dfcc1037fb596a0c831b76f3f5fabd1
Toncho <toncho.lozev@yahoo.com>
2020-11-16 21:06:21 +0200

    hash table

---
+++ hash-table/01.HashTable.js
@@ -0,0 +1,95 @@
+function HashTable(size) {
+    this.buckets = Array(size);
+    this.numBuckets = this.buckets.length;
+}
+
+function HashNode(key, value, next) {
+    this.key = key;
+    this.value = value;
+    this.next = next || null;
+}
+
+HashTable.prototype.hash = function(key) {
+    let total = 0;
+
+    for (let i = 0; i< key.length; i++){
+        total += key.charCodeAt(i);
+    }
+
+    const bucket = total % this.numBuckets;
+
+    return bucket;
+}
+
+HashTable.prototype.insert = function(key, value) {
+    const index = this.hash(key);
+
+    if(!this.buckets[index]) {
+        this.buckets[index] = new HashNode(key, value);
+    } else if(this.buckets[index].key === key) {
+        this.buckets[index].value = value;
+    } else { 
+        let currentNode = this.buckets[index];
+
+        while(currentNode.next) {
+            if(currentNode.next.key === key) {
+                currentNode.next.value = value;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = new HashNode(key, value);
+    }
+}
+
+HashTable.prototype.get = function(key) {
+    const index = this.hash(key);
+
+    if(!this.buckets[index]) {
+        return null;
+    } else {
+        let currentNode = this.buckets[index];
+
+        while(currentNode) {
+            if(currentNode.key === key) {
+                return currentNode.value;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return null;
+    }
+}
+
+HashTable.prototype.retrieveAll = function() {
+    let nodesArray = [];
+
+    for(let i = 0; i < this.buckets.length; i++) {
+        let currentNode = this.buckets[i];
+
+        while(currentNode){
+            nodesArray.push(currentNode);
+            currentNode = currentNode.next;
+        }
+    }
+
+    return nodesArray;
+}
+
+const myHT = new HashTable(30);
+
+myHT.insert('Dean', 'dean@gmail.com');
+myHT.insert('Megan', 'megan@gmail.com');
+myHT.insert('Dane', 'dane@gmail.com');
+myHT.insert('Dean', 'deanmachine@gmail.com');
+myHT.insert('Megan', 'megansmith@gmail.com');
+myHT.insert('Dane', 'dane1010@outlook.com');
+
+console.log(myHT.get('Dean'));
+console.log(myHT.get('Megan'));
+console.log(myHT.get('Dane'));
+
+console.log(myHT.retrieveAll());
+
